7c8c11880c145a369483f999e3bed92117274963
chaerim <qkrdbsk28@naver.com>
2023-01-13 01:25:19 +0900

    feat: project initial settings

---
+++ src/test/java/com/dnd/wedding/WeddingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd.wedding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
