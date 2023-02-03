bd5bd03a22b18291b0170d6d41d5b5bf961fd8e0
Jeonghun-Ban <junghoon.ban@gmail.com>
2023-02-03 20:26:07 +0900

    test(oauth2): use mockito to replace given data

--- src/test/java/com/dnd/wedding/domain/oauth/info/OAuth2UserInfoFactoryTest.java
+++ src/test/java/com/dnd/wedding/domain/oauth/info/OAuth2UserInfoFactoryTest.java
@@ -1,8 +1,11 @@
 package com.dnd.wedding.domain.oauth.info;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 
 import com.dnd.wedding.domain.oauth.OAuth2Provider;
+import com.dnd.wedding.domain.oauth.info.impl.GoogleOAuth2UserInfo;
 import com.dnd.wedding.domain.oauth.info.impl.KakaoOAuth2UserInfo;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
@@ -14,10 +17,13 @@ class OAuth2UserInfoFactoryTest {
     // given
     OAuth2Provider kakao = OAuth2Provider.KAKAO;
 
-    Map<String, Object> profile = Map.of("nickname", "test", "profile_image_url", "test");
-    Map<String, Object> kakaoAccount = Map.of("email", "test@example.com");
+    Map<String, Object> profile = Map.of(
+        "nickname", anyString(),
+        "profile_image_url", anyString());
+    Map<String, Object> kakaoAccount = Map.of(
+        "email", anyString());
     Map<String, Object> attributes = Map.of(
-        "id", "1234", "kakao_account", kakaoAccount, "profile", profile);
+        "id", anyInt(), "kakao_account", kakaoAccount, "profile", profile);
 
     // when
     OAuth2UserInfo userInfo = OAuth2UserInfoFactory.getOAuth2Userinfo(kakao, attributes);
