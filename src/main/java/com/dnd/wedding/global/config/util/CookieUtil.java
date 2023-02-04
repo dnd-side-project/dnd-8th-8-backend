34a3b093947e5414c7c8aa991f43af2507d6d76a
Jeonghun-Ban <junghoon.ban@gmail.com>
2023-02-04 13:52:09 +0900

    refactor(CookieUtil): simplify conditional statement

--- src/main/java/com/dnd/wedding/global/config/util/CookieUtil.java
+++ src/main/java/com/dnd/wedding/global/config/util/CookieUtil.java
@@ -11,7 +11,7 @@ public class CookieUtil {
 
   public static Optional<Cookie> getCookie(HttpServletRequest request, String name) {
     Cookie[] cookies = request.getCookies();
-    if (cookies != null && cookies.length > 0) {
+    if (cookies != null) {
       for (Cookie cookie : cookies) {
         if (cookie.getName().equals(name)) {
           return Optional.of(cookie);
@@ -34,7 +34,7 @@ public class CookieUtil {
   public static void deleteCookie(HttpServletRequest request, HttpServletResponse response,
       String name) {
     Cookie[] cookies = request.getCookies();
-    if (cookies != null && cookies.length > 0) {
+    if (cookies != null) {
       for (Cookie cookie : cookies) {
         if (cookie.getName().equals(name)) {
           cookie.setValue("");
