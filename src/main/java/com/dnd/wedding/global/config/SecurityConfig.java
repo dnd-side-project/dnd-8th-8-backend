7a0895ff6180ce1c31879f89f0b54343aa487d74
chaerim <qkrdbsk28@naver.com>
2023-02-11 00:27:51 +0900

    feat: change csrf setting to disabled

--- src/main/java/com/dnd/wedding/global/config/SecurityConfig.java
+++ src/main/java/com/dnd/wedding/global/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.cors()
         .and()
+        .csrf().disable()
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
     http.authorizeHttpRequests()
