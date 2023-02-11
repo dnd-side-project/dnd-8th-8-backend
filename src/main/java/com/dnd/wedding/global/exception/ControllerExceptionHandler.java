754a8289c21645ca6066902344520b51dd73699b
chaerim <qkrdbsk28@naver.com>
2023-02-12 01:11:11 +0900

    feat: add additional exception handler

--- src/main/java/com/dnd/wedding/global/exception/ControllerExceptionHandler.java
+++ src/main/java/com/dnd/wedding/global/exception/ControllerExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.dnd.wedding.global.exception;
 
-import java.util.Date;
+import com.dnd.wedding.domain.oauth.exception.BadRequestException;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,5 +17,42 @@ public class ControllerExceptionHandler {
         ex.getMessage());
 
     return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(BadRequestException.class)
+  public ResponseEntity<ErrorMessage> badRequestException(BadRequestException ex) {
+    ErrorMessage message = new ErrorMessage(
+        HttpStatus.BAD_REQUEST.value(),
+        ex.getMessage());
+
+    return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(UnauthorizedException.class)
+  public ResponseEntity<ErrorMessage> unauthorizedUserException(UnauthorizedException ex) {
+    ErrorMessage message = new ErrorMessage(
+        HttpStatus.UNAUTHORIZED.value(),
+        ex.getMessage());
+
+    return new ResponseEntity<>(message, HttpStatus.UNAUTHORIZED);
+  }
+
+  @ExceptionHandler(ForbiddenException.class)
+  public ResponseEntity<ErrorMessage> forbiddenRequestException(ForbiddenException ex) {
+    ErrorMessage message = new ErrorMessage(
+        HttpStatus.FORBIDDEN.value(),
+        ex.getMessage());
+
+    return new ResponseEntity<>(message, HttpStatus.FORBIDDEN);
+  }
+
+  @ExceptionHandler(InternalServerErrorException.class)
+  public ResponseEntity<ErrorMessage> internalServerErrorException(
+      InternalServerErrorException ex) {
+    ErrorMessage message = new ErrorMessage(
+        HttpStatus.INTERNAL_SERVER_ERROR.value(),
+        ex.getMessage());
+
+    return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
   }
 }
---
+++ src/main/java/com/dnd/wedding/global/exception/InternalServerErrorException.java
@@ -0,0 +1,12 @@
+package com.dnd.wedding.global.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "InternalServerError")
+public class InternalServerErrorException extends RuntimeException {
+
+  public InternalServerErrorException(String message) {
+    super(message);
+  }
+}
