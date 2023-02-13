dd9c470dcbac33a0ad5f76d5540cd154dc52d527
chaerim <qkrdbsk28@naver.com>
2023-02-13 17:59:41 +0900

    feat: add get checklist api

--- src/main/java/com/dnd/wedding/domain/checklist/checklistitem/repository/ChecklistItemRepository.java
+++ src/main/java/com/dnd/wedding/domain/checklist/checklistitem/repository/ChecklistItemRepository.java
@@ -1,10 +1,13 @@
 package com.dnd.wedding.domain.checklist.checklistitem.repository;
 
 import com.dnd.wedding.domain.checklist.checklistitem.ChecklistItem;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChecklistItemRepository extends JpaRepository<ChecklistItem, Long> {
 
   Optional<ChecklistItem> findOneById(Long id);
+
+  List<ChecklistItem> findByMemberId(Long id);
 }
