bd6623dfafdeaf47d3edc2c9b23037583ebd0a11
Jeonghun-Ban <junghoon.ban@gmail.com>
2023-02-16 13:37:44 +0900

    feat(util): add header util

---
+++ src/main/java/com/dnd/wedding/global/util/HeaderUtil.java
@@ -0,0 +1,23 @@
+package com.dnd.wedding.global.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+public class HeaderUtil {
+
+  private static final String HEADER_AUTHORIZATION = "Authorization";
+  private static final String TOKEN_PREFIX = "Bearer ";
+
+  private HeaderUtil() {
+    throw new IllegalStateException("Utility class");
+  }
+
+  public static String parseBearerToken(HttpServletRequest request) {
+    String bearerToken = request.getHeader(HEADER_AUTHORIZATION);
+
+    if (bearerToken.startsWith(TOKEN_PREFIX)) {
+      return bearerToken.substring(TOKEN_PREFIX.length());
+    }
+
+    return null;
+  }
+}
