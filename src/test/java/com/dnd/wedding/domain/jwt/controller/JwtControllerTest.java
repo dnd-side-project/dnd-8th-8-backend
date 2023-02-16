eb7f517caeec366a4fd646e6a0977f47bc9d9b04
Jeonghun-Ban <junghoon.ban@gmail.com>
2023-02-16 22:03:47 +0900

    test(jwt): define constant value

--- src/test/java/com/dnd/wedding/domain/jwt/controller/JwtControllerTest.java
+++ src/test/java/com/dnd/wedding/domain/jwt/controller/JwtControllerTest.java
@@ -23,10 +23,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.servlet.ResultActions;
 
 @WebMvcTest(JwtController.class)
 class JwtControllerTest extends AbstractRestDocsTests {
+
+  final String ACCESS_TOKEN_PREFIX = "Bearer ";
+  final String REFRESH_TOKEN_NAME = "refresh";
 
   @MockBean
   JwtTokenProvider jwtTokenProvider;
@@ -40,22 +44,22 @@ class JwtControllerTest extends AbstractRestDocsTests {
 
     // given
     given(jwtService.refreshToken(
-        any(HttpServletRequest.class), any(HttpServletResponse.class), eq("accessToken")
-    )).willReturn("newAccessToken");
+        any(HttpServletRequest.class), any(HttpServletResponse.class), eq("OLD_ACCESS_TOKEN")
+    )).willReturn("NEW_ACCESS_TOKEN");
 
     // when
     ResultActions result = mockMvc.perform(post("/api/v1/jwt/refresh")
-            .cookie(new Cookie("refresh", "refreshToken"))
-            .header("Authorization", "Bearer " + "accessToken"));
+            .cookie(new Cookie(REFRESH_TOKEN_NAME, "OLD_REFRESH_TOKEN"))
+            .header(HttpHeaders.AUTHORIZATION, ACCESS_TOKEN_PREFIX + "OLD_ACCESS_TOKEN"));
 
     // then
     result.andExpect(status().isOk())
         .andDo(document("jwt/refresh",
             requestCookies(
-                cookieWithName("refresh").description("refresh token")
+                cookieWithName(REFRESH_TOKEN_NAME).description("refresh token")
             ),
             requestHeaders(
-                headerWithName("Authorization").description("access token")
+                headerWithName(HttpHeaders.AUTHORIZATION).description("access token")
             ),
             responseFields(
                 fieldWithPath("status").description("응답 상태 코드"),
