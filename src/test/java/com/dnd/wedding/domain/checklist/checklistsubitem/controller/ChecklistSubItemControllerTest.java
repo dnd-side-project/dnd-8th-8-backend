8d79e2294e6b6d873bf7a2a78b7c3231741382be
chaerim <qkrdbsk28@naver.com>
2023-02-16 17:01:59 +0900

    test: add ChecklistSubItemControllerTest

---
+++ src/test/java/com/dnd/wedding/domain/checklist/checklistsubitem/controller/ChecklistSubItemControllerTest.java
@@ -0,0 +1,209 @@
+package com.dnd.wedding.domain.checklist.checklistsubitem.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.beneathPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.dnd.wedding.docs.springrestdocs.AbstractRestDocsTests;
+import com.dnd.wedding.domain.checklist.checklistitem.ChecklistItem;
+import com.dnd.wedding.domain.checklist.checklistitem.service.ChecklistItemService;
+import com.dnd.wedding.domain.checklist.checklistsubitem.ChecklistSubItem;
+import com.dnd.wedding.domain.checklist.checklistsubitem.dto.ChecklistSubItemDto;
+import com.dnd.wedding.domain.checklist.checklistsubitem.dto.UpdateChecklistSubItemDto;
+import com.dnd.wedding.domain.checklist.checklistsubitem.service.ChecklistSubItemService;
+import com.dnd.wedding.domain.jwt.JwtTokenProvider;
+import com.dnd.wedding.domain.member.MemberRepository;
+import com.dnd.wedding.global.WithMockOAuth2User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@AutoConfigureRestDocs
+@ExtendWith(RestDocumentationExtension.class)
+@WebMvcTest(ChecklistSubItemController.class)
+class ChecklistSubItemControllerTest extends AbstractRestDocsTests {
+
+  static final Long CHECKLIST_ITEM_ID = 1L;
+  static final Long CHECKLIST_SUB_ITEM_ID = 1L;
+
+  @Autowired
+  MockMvc mockMvc;
+  @MockBean
+  ChecklistItemService checklistItemService;
+  @MockBean
+  ChecklistSubItemService checklistSubItemService;
+  @MockBean
+  MemberRepository memberRepository;
+  @MockBean
+  JwtTokenProvider jwtTokenProvider;
+
+  private ObjectMapper objectMapper;
+
+  private final ChecklistItem checklistItem = ChecklistItem.builder()
+      .id(1L)
+      .title("title")
+      .checkDate(LocalDate.now())
+      .time(LocalTime.now())
+      .place("place")
+      .memo("memo")
+      .build();
+
+  private final ChecklistSubItem checklistSubItem = ChecklistSubItem.builder()
+      .id(1L)
+      .contents("contents")
+      .isChecked(false)
+      .build();
+
+  @BeforeEach
+  void init() {
+    objectMapper = new ObjectMapper();
+    objectMapper.registerModule(new JavaTimeModule());
+  }
+
+  @Test
+  @WithMockOAuth2User
+  @DisplayName("체크리스트 서브 아이템 생성")
+  void createChecklistSubItem() throws Exception {
+    String url = "/api/v1/checklist/item/{item-id}/sub-item";
+
+    ChecklistSubItemDto createChecklistSubItemDto = ChecklistSubItemDto.builder()
+        .contents("contents")
+        .build();
+
+    when(checklistItemService.findChecklistItemById(anyLong())).thenReturn(
+        Optional.ofNullable(checklistItem));
+    when(checklistSubItemService.saveChecklistSubItem(any(ChecklistSubItemDto.class),
+        any(ChecklistItem.class))).thenReturn(checklistSubItem);
+
+    mockMvc.perform(post(url, CHECKLIST_ITEM_ID)
+            .with(csrf())
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(createChecklistSubItemDto)))
+        .andExpect(status().isCreated())
+        .andDo(print())
+        .andDo(
+            document("checklist/sub-item/checklist-sub-item-create",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                pathParameters(
+                    parameterWithName("item-id").description("체크리스트 아이템 아이디")
+                ), requestFields(
+                    fieldWithPath("id").ignored(),
+                    fieldWithPath("contents").description("등록할 체크리스트 서브 아이템 내용").type(
+                        JsonFieldType.STRING),
+                    fieldWithPath("isChecked").ignored()
+                ),
+                responseFields(
+                    beneathPath("data").withSubsectionId("data"),
+                    fieldWithPath("id").description("등록된 체크리스트 서브 아이템 아이디").type(
+                        JsonFieldType.NUMBER),
+                    fieldWithPath("contents").description("등록된 체크리스트 서브 아이템 내용")
+                        .type(
+                            JsonFieldType.STRING),
+                    fieldWithPath("isChecked").description(
+                        "등록된 체크리스트 서브 아이템 체크 여부").type(
+                        JsonFieldType.BOOLEAN)
+                )));
+  }
+
+  @Test
+  @WithMockOAuth2User
+  @DisplayName("체크리스트 서브 아이템 체크 여부 수정")
+  void modifyChecklistSubItem() throws Exception {
+    String url = "/api/v1/checklist/item/{item-id}/sub-item/{sub-item-id}";
+
+    UpdateChecklistSubItemDto updateChecklistSubItemDto = UpdateChecklistSubItemDto.builder()
+        .isChecked(false)
+        .build();
+    when(checklistItemService.findChecklistItemById(anyLong())).thenReturn(
+        Optional.ofNullable(checklistItem));
+    when(checklistSubItemService.modifyChecklistSubItem(anyLong(),
+        anyLong(), any(UpdateChecklistSubItemDto.class))).thenReturn(checklistSubItem);
+
+    mockMvc.perform(put(url, CHECKLIST_ITEM_ID, CHECKLIST_SUB_ITEM_ID)
+            .with(csrf())
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(updateChecklistSubItemDto)))
+        .andExpect(status().isOk())
+        .andDo(print())
+        .andDo(
+            document("checklist/sub-item/checklist-sub-item-modify",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                pathParameters(
+                    parameterWithName("item-id").description("체크리스트 아이템 아이디"),
+                    parameterWithName("sub-item-id").description("체크리스트 서브 아이템 아이디")
+                ), requestFields(
+                    fieldWithPath("isChecked").description("수정할 체크리스트 서브 아이템 체크 여부").type(
+                        JsonFieldType.BOOLEAN)
+                ),
+                responseFields(
+                    beneathPath("data").withSubsectionId("data"),
+                    fieldWithPath("id").description("수정된 체크리스트 서브 아이템 아이디").type(
+                        JsonFieldType.NUMBER),
+                    fieldWithPath("contents").description("수정된 체크리스트 서브 아이템 내용").type(
+                        JsonFieldType.STRING),
+                    fieldWithPath("isChecked").description(
+                        "수정된 체크리스트 서브 아이템 체크 여부").type(
+                        JsonFieldType.BOOLEAN)
+                )
+            ));
+  }
+
+  @Test
+  @WithMockOAuth2User
+  @DisplayName("체크리스트 서브 아이템 삭제")
+  void withdrawChecklistSubItem() throws Exception {
+    String url = "/api/v1/checklist/item/{item-id}/sub-item/{sub-item-id}";
+
+    when(checklistItemService.findChecklistItemById(anyLong())).thenReturn(
+        Optional.ofNullable(checklistItem));
+    when(checklistSubItemService.withdrawChecklistSubItem(anyLong(), anyLong())).thenReturn(true);
+
+    mockMvc.perform(delete(url, CHECKLIST_ITEM_ID, CHECKLIST_SUB_ITEM_ID)
+            .with(csrf()))
+        .andExpect(status().isOk())
+        .andDo(print())
+        .andDo(
+            document("checklist/sub-item/checklist-sub-item-withdraw",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                pathParameters(
+                    parameterWithName("item-id").description("체크리스트 아이템 아이디"),
+                    parameterWithName("sub-item-id").description("체크리스트 서브 아이템 아이디")
+                )
+            ));
+  }
+}
