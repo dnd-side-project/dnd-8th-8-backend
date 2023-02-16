273ea2755893188ab20a29a16b6c8b4de1f6423b
Jeonghun-Ban <junghoon.ban@gmail.com>
2023-02-16 10:17:26 +0900

    test(jwt): update jwt controller test for documentation

--- src/test/java/com/dnd/wedding/domain/jwt/controller/JwtControllerTest.java
+++ src/test/java/com/dnd/wedding/domain/jwt/controller/JwtControllerTest.java
@@ -1,57 +1,61 @@
 package com.dnd.wedding.domain.jwt.controller;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
+import static org.springframework.restdocs.cookies.CookieDocumentation.cookieWithName;
+import static org.springframework.restdocs.cookies.CookieDocumentation.requestCookies;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.dnd.wedding.docs.springrestdocs.AbstractRestDocsTests;
+import com.dnd.wedding.domain.jwt.JwtTokenProvider;
 import com.dnd.wedding.domain.jwt.service.JwtService;
-import org.junit.jupiter.api.BeforeEach;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-class JwtControllerTest {
+@WebMvcTest(JwtController.class)
+class JwtControllerTest extends AbstractRestDocsTests {
 
-  JwtController jwtController;
+  @MockBean
+  JwtTokenProvider jwtTokenProvider;
+
+  @MockBean
   JwtService jwtService;
-  MockHttpServletRequest request;
-  MockHttpServletResponse response;
-
-  @BeforeEach
-  void init() {
-    jwtService = mock(JwtService.class);
-    jwtController = new JwtController(jwtService);
-    request = new MockHttpServletRequest();
-    response = new MockHttpServletResponse();
-  }
 
   @Test
   @DisplayName("access token 갱신 성공 시 새로 발급한 token을 전달한다.")
-  void successRenewAccessToken() {
-    // given
-    given(jwtService.refreshToken(request, response, "accessToken")).willReturn("token");
+  void refresh() throws Exception {
 
-    // when
-    ResponseEntity responseEntity = jwtController.refreshToken(request, response, "accessToken");
+    Cookie cookie = new Cookie("refresh", "refreshToken");
+    given(jwtService.refreshToken(
+        any(HttpServletRequest.class), any(HttpServletResponse.class), eq("accessToken")
+    )).willReturn("newAccessToken");
 
-    // then
-    assertEquals("token", responseEntity.getBody());
-    assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-  }
-
-  @Test
-  @DisplayName("access token 갱신 실패 시 401 코드가 전달된다.")
-  void failRenewAccessToken() {
-    // given
-    given(jwtService.refreshToken(request, response, "accessToken")).willReturn(null);
-
-    // when
-    ResponseEntity responseEntity = jwtController.refreshToken(request, response, "accessToken");
-
-    // then
-    assertEquals(HttpStatus.UNAUTHORIZED, responseEntity.getStatusCode());
+    mockMvc.perform(get("/api/v1/jwt/refresh?accessToken=accessToken").cookie(cookie))
+        .andExpect(status().isOk())
+        .andDo(document("jwt/refresh",
+            requestCookies(
+                cookieWithName("refresh").description("refresh token")
+            ),
+            queryParameters(
+                parameterWithName("accessToken").description("갱신할 access token")
+            ),
+            responseFields(
+                fieldWithPath("status").description("응답 상태 코드"),
+                fieldWithPath("message").description("응답 메시지"),
+                fieldWithPath("data").description("응답 데이터")
+            )
+        ));
   }
 }
