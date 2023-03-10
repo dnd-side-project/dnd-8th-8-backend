e7659c3280f13d638c28118c492c7d4ad423f5b1
Jeonghun-Ban <junghoon.ban@gmail.com>
2023-03-10 13:38:20 +0900

    refactor(controller): reorder annotations

--- src/main/java/com/dnd/weddingmap/domain/jwt/controller/JwtController.java
+++ src/main/java/com/dnd/weddingmap/domain/jwt/controller/JwtController.java
@@ -14,8 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("api/v1/jwt")
+@RequiredArgsConstructor
 public class JwtController {
 
   private final JwtService jwtService;
