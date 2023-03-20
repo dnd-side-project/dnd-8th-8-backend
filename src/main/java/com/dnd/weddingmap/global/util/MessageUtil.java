5d666728392841c9a070cd6bdb021dc477e26f44
Jeonghun-Ban <junghoon.ban@gmail.com>
2023-03-20 13:51:51 +0900

    refactor(message): use autowired annotation to inject message source

--- src/main/java/com/dnd/weddingmap/global/util/MessageUtil.java
+++ src/main/java/com/dnd/weddingmap/global/util/MessageUtil.java
@@ -1,12 +1,19 @@
 package com.dnd.weddingmap.global.util;
 
 import java.util.Locale;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
 
-@RequiredArgsConstructor
+@Component
 public class MessageUtil {
-  private static MessageSource messageSource;
+
+  static MessageSource messageSource;
+
+  @Autowired
+  private MessageUtil(MessageSource messageSource) {
+    MessageUtil.messageSource = messageSource;
+  }
 
   public static String getMessage(String code) {
     return messageSource.getMessage(code, null, Locale.getDefault());
