62aef6056cc2ea3a46aa8d7bead76f3016e73c0a
Jeonghun-Ban <junghoon.ban@gmail.com>
2023-03-20 13:52:37 +0900

    refactor(message): add getter/setter for message source

--- src/main/java/com/dnd/weddingmap/global/util/MessageUtil.java
+++ src/main/java/com/dnd/weddingmap/global/util/MessageUtil.java
@@ -8,18 +8,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class MessageUtil {
 
-  static MessageSource messageSource;
+  private static MessageSource messageSource;
 
   @Autowired
   private MessageUtil(MessageSource messageSource) {
-    MessageUtil.messageSource = messageSource;
+    MessageUtil.setMessageSource(messageSource);
   }
 
   public static String getMessage(String code) {
-    return messageSource.getMessage(code, null, Locale.getDefault());
+    return getMessageSource().getMessage(code, null, Locale.getDefault());
   }
 
   public static String getMessage(String code, Object[] args) {
-    return messageSource.getMessage(code, args, Locale.getDefault());
+    return getMessageSource().getMessage(code, args, Locale.getDefault());
+  }
+
+  public static MessageSource getMessageSource() {
+    return messageSource;
+  }
+
+  public static void setMessageSource(MessageSource messageSource) {
+    MessageUtil.messageSource = messageSource;
   }
 }
